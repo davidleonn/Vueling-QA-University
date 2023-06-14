c8cd0864e2fec9a44c357d4911615739bc3254e1
davidleonn <dleon.kenos@hotmail.com>
2023-06-14 09:13:04 +0200

    Upload cypress template done in class, its the empty template it lacks examples and some formatting

---
+++ Cypress/Cypress Template/cypress/e2e/TemplateTest.cy.js
@@ -0,0 +1,32 @@
+import { SearchWebCheckin } from "../webpages/searchWebCheckin"; // Webpage Import
+
+// The container of the tests (must contain the same name as the file)
+describe("TemplateTest", () => {
+  // * let/const for all the tests
+
+  const searchWebCheckin = new SearchWebCheckin(); // Object of the webpage
+
+  // This will be executed only once before and for all the tests
+  before(() => {});
+
+  // This will be executed before the execution of every test
+  beforeEach(() => {
+    // Must be included to go to the specified URL
+    cy.visit("");
+  });
+
+  // Independent Test Case
+  it("Verify the error - Invalid PNR (PNR + Email)", () => {
+    // Path added to the base URL
+    cy.visit("/index.html");
+  });
+
+  // This will be executed only once after and for all the tests
+  after(() => {});
+
+  // This will be executed after the execution of every test
+  afterEach(() => {
+    // This will save a screenshot into the screenshots folder
+    cy.screenshot(`Screenshot_PNR_${pnr}`);
+  });
+});
